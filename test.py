f7bcae346633bdf75256bd25a15952640edf6e19
ThomasPetkovic <thomas.petkovic@ontariotechu.net>
2024-02-02 23:20:16 -0500

    thomas change

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title("File")
+root.title("File Dialog Box"
 root.minsize(200, 200)  # width, height
 root.geometry("500x500+50+50")
 
@@ -10,3 +10,4 @@ image = PhotoImage(file="caption.gif")
 img = Label(root, image=image)
 img.pack()
 root.mainloop()
+
