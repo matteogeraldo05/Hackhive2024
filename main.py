d523a9432a41328f522208f09984fa5eaf6fbec3
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-02-03 10:08:19 -0500

    added stuff

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+import time
 #git add --all
 #git commit -m "hackhive"
 #git push origin main 
@@ -35,9 +37,6 @@ emergency contacty(str)
 
 class Patient:
     def __init__(self, firstName, lastName, birthday, married, checkIn, phone, address, email, doctor, occupation, allergies, conditions, sex, ethnicity, image, insurance, credit_card, height, weight, emergency_contact):
-    
-
-
         self.firstName = firstName
         self.lastName = lastName
         self.birthday = birthday
@@ -87,16 +86,33 @@ class Patient:
     def create_patient(self):
         return self.patient_dict
     
-
-
+    #check if users phone number configuration is valid
+    def validate_phone():
+        #'905-721-8668' OR '9057218668'
+        while not re.match("^(?!.*\(\d{3}\)\d{3}-\d{4}$)\(?\d{3}\)?-?\d{3}-?\d{4}$", phone):
+            print("Invalid phone number format. Please try again.")
+            phone = input("Enter Phone Number: ")
+        return phone
+    
+    #check if user email is valid
+    def validate_email(self, email):
+        while not re.match("[^@]+@[^@]+\.[^@]+", email):
+            print("Invalid email format. Please try again.")
+            email = input("Enter Email: ")
+        return email
+        
+def current_time():
+    localTime = time.localtime()
+    current_time = time.strftime("%H:%M:%S", localTime)
+    return current_time
 
 for i in range(1):
     firstName = input("Enter First Name: ")
     lastName = input("Enter Last Name: ")
     birthday = input("Enter Birthday: ")
     married = input("Married? YES or NO: ")
-    checkIn = input("Enter Check In Time: ")
-    phone = int(input("Enter Phone Number: "))
+    checkIn = current_time()
+    phone = input("Enter Phone Number: ")
     address = input("Enter Address: ")
     email = input("Enter Email: ")
     doctor = input("Enter Doctor: ")
